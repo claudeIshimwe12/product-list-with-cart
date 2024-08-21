c825992865721b2c103afc9e3f65d24c54776035
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-08-21 12:25:56 +0200

    quick-fix, home route redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
